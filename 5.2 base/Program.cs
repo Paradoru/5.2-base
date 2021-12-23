4833665e46b629658962a9845b6e46485facecc2
Paradoru <LicLexa@yandex.ru>
2021-12-24 00:06:07 +0200

    Добавьте файлы проекта.

---
+++ 5.2 base/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Lib;
+
+namespace _5._2_base
+    // базовый уровень 9 вариант
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Random r = new Random();
+                int[,] Mass = new int[6, 6];
+                int i, j;
+                string byf;
+                Console.Write("1 - Ввод вручную, 2 - Рандомный ввод: ");
+                byf = Console.ReadLine();
+                switch (byf)
+                {
+                    case "1":
+                        for (i = 0; i < Mass.GetLength(0); ++i)
+                            for (j = 0; j < Mass.GetLength(1); ++j)
+                            {
+                                Console.Write("Введите " + i + " " + j + " элемент: ");
+                                Mass[i, j] = int.Parse(Console.ReadLine());
+                            }
+                        break;
+
+                    case "2":
+                        for (i = 0; i < Mass.GetLength(0); i++)
+                            for (j = 0; j < Mass.GetLength(1); j++)
+                                Mass[i, j] = r.Next(-100, 100);
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.ReadKey();
+                        return;
+                }
+                int res = Lib.Class1.Sum(Mass);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class Class1
+    {
+        public static int Sum(int[,] Mass)
+        {
+            int s = 1;
+            Console.Write("Массив:");
+            for (int i = 0; i < Mass.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < Mass.GetLength(1); j++)
+                {
+                    Console.Write(Mass[i, j] + " ");
+                    if (i == j) s *= Mass[i, j];
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Произведение элементов " + s);
+            Console.ReadKey();
+            return s;
+        }
+    }
+}
